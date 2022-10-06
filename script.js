126f56627dfe2d73c0b2fb51790b0e3575741888
David Chege <dkc.ushift@gmail.com>
2022-10-06 00:35:26 -0500

    Last Changes

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
+
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    // p = document.getElementById("principal").value;   
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    // document.getElementById("interest_result").innerText=interest
+    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
 }
-        
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=`${rateval}%`;
+}
+
+function checkdata()
+{
+    var principal = document.getElementById("principal").value;
+
+  
+      if(principal <= 0 ){
+            alert("Enter a positive number");
+            document.getElementById("principal").focus();
+            return false
+           
+      }
+
+    return true
+
+}
